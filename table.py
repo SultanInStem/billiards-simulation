8fc2f29a556b14a60266982847ba897b65c9ca50
= <sultaninstem004@gmail.com>
2025-01-05 20:00:24 -0800

    table drawn

--- table.py
+++ table.py
@@ -8,12 +8,11 @@ class Table:
         self.type = type
         self.line_top = Line((-radius, radius), (radius, radius), color)
         self.line_bottom = Line((-radius, -radius), (radius, -radius), color)
-        self.arc_left = Arc((0,-2*radius), 1*radius, math.pi / 2, 3*math.pi / 2, color)
 
-         
         self.arc_right = Arc((radius, 0), 2*radius, 3 *math.pi / 2, math.pi / 2, color)
+        self.arc_left = Arc((-radius, 0), 2*radius, math.pi / 2, 3*math.pi / 2, color)
 
-        self.shapes = [self.line_top, self.line_bottom, self.arc_right]
+        self.shapes = [self.line_top, self.line_bottom, self.arc_right, self.arc_left]
     def draw(self, screen):
         for shape in self.shapes: 
             shape.draw(screen)
